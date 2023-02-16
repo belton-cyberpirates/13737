3ecfcd49f928ef24e27f4abe231303ee26c93e6a
M Cooper Healy <m.cooper.healy@gmail.com>
2023-02-16 09:47:56 -0600

    Add Webcam initialization

--- AutoLeft13737.java
+++ AutoLeft13737.java
@@ -112,6 +112,11 @@ public class AutoLeft13737 extends LinearOpMode {
    */
   @Override
   public void runOpMode() {
+    
+    WebcamName camName = hardwareMap.get(WebcamName.class, "Webcam 1");
+    camera = OpenCvCameraFactory.getInstance().createWebcam(camName);
+    aprilTagDetectionPipeline = new AprilTagDetectionPipeline(Config.TAGSIZE, Config.FX, Config.FY, Config.CX, Config.CY);
+    
     // argument order *must* be fr-fl-bl-br
     driveMotors = new DriveMotors(
       hardwareMap.get(DcMotorEx.class, "m2"),
