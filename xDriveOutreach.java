c78862ca5f79a82582c170d83d1b1398bb766358
Oshawhat <85427862+Oshawhaat@users.noreply.github.com>
2023-04-25 08:33:54 -0400

    add outreach drive code

--- xDriveOutreach.java
+++ xDriveOutreach.java
@@ -9,8 +9,8 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import org.firstinspires.ftc.robotcore.external.android.AndroidTextToSpeech;
 
-@TeleOp(name = "xDriveComp")
-public class xDriveCode extends LinearOpMode {
+@TeleOp(name = "xDriveOutreach")
+public class xDriveOutreach extends LinearOpMode {
 
   private DcMotorEx m1;
   private DcMotorEx m2;
@@ -18,11 +18,15 @@ public class xDriveCode extends LinearOpMode {
   private DcMotorEx m4;
   private DcMotorEx arm2;
   private DcMotorEx claw;
-  private AndroidTextToSpeech androidTextToSpeech;
   private AnalogInput armpot;
   private DcMotorEx arm1;
   private AnalogInput clawpot;
   private double clawComp = 0;
+
+  private void setArmPower(double power) {
+    arm1.setPower(power);
+    arm2.setPower(power);
+  }
 
   /**
    * This function is executed when this Op Mode is selected from the Driver Station.
@@ -31,6 +35,7 @@ public class xDriveCode extends LinearOpMode {
   public void runOpMode() {
     int stayClosed;
     double compensation = 1;
+    boolean boostEnabled = false;
 
     m1 = hardwareMap.get(DcMotorEx.class, "m1");
     m2 = hardwareMap.get(DcMotorEx.class, "m2");
@@ -38,7 +43,6 @@ public class xDriveCode extends LinearOpMode {
     m4 = hardwareMap.get(DcMotorEx.class, "m4");
     arm2 = hardwareMap.get(DcMotorEx.class, "arm2");
     claw = hardwareMap.get(DcMotorEx.class, "claw");
-    androidTextToSpeech = new AndroidTextToSpeech();
     armpot = hardwareMap.get(AnalogInput.class, "armpot");
     arm1 = hardwareMap.get(DcMotorEx.class, "arm1");
     clawpot = hardwareMap.get(AnalogInput.class, "clawpot");
@@ -52,8 +56,7 @@ public class xDriveCode extends LinearOpMode {
     arm2.setDirection(DcMotorSimple.Direction.REVERSE);
     claw.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     stayClosed = 0;
-    double speed = 1100;
-    androidTextToSpeech.initialize();
+    double speed = 750;
     if (opModeIsActive()) {
       while (opModeIsActive()) {
         telemetry.addData("clawComp", clawComp);
@@ -61,7 +64,7 @@ public class xDriveCode extends LinearOpMode {
         double leftTriggerCorrection  = gamepad1.left_trigger > 0.2 ? 0.2 - (gamepad1.left_trigger * .7) : 0; // if left trigger, slow down
         double armCorrection = armpot.getVoltage() < 0.715 ? - 0.3 : 0; // if arm up, slow down
 
-        double SpeedupVal = 1 + rightTriggerCorrection + leftTriggerCorrection + armCorrection;
+        double SpeedupVal = 1 + boostEnabled ? (rightTriggerCorrection + leftTriggerCorrection + armCorrection) : 0;
 
         telemetry.addData("gamepad info", gamepad2);
 
@@ -70,28 +73,21 @@ public class xDriveCode extends LinearOpMode {
         m4.setVelocity((int)(((-gamepad1.left_stick_y + -gamepad1.left_stick_x + gamepad1.right_stick_x) * SpeedupVal) * speed));
         m1.setVelocity((int)(((gamepad1.left_stick_y + -gamepad1.left_stick_x + gamepad1.right_stick_x) * SpeedupVal) * speed * compensation));
         if (gamepad2.right_stick_y < 0) {
-          arm1.setPower(-(gamepad2.right_stick_y / 2));
-          arm2.setPower(-(gamepad2.right_stick_y / 2));
+          setArmPower(-(gamepad2.right_stick_y * .9));
         } else if (gamepad2.right_stick_y > 0) {
-          arm1.setPower(-(gamepad2.right_stick_y / 3));
-          arm2.setPower(-(gamepad2.right_stick_y / 3));
+          setArmPower(-(gamepad2.right_stick_y * .5));
         } else if (gamepad2.left_bumper) {
-          arm1.setPower(-0.5);
-          arm2.setPower(-0.5);
+          setArmPower(-0.5);
         } else if (gamepad2.right_bumper) {
           if (armpot.getVoltage() < 0.5) {
-            arm1.setPower(0.3);
-            arm2.setPower(0.3);
+            setArmPower(0.3)
           } else {
-            arm1.setPower(0.9);
-            arm2.setPower(0.9);
+            setArmPower(0.9);
           }
         } else if (armpot.getVoltage() > 1.45) {
-          arm1.setPower(0);
-          arm2.setPower(0);
+          setArmPower(0)
         } else {
-          arm1.setPower(0.1);
-          arm2.setPower(0.1);
+          setArmPower(0.1)
         }
         if (clawpot.getVoltage() < 1.35 + clawComp) {
           claw.setPower(0.3);
@@ -106,20 +102,10 @@ public class xDriveCode extends LinearOpMode {
         } else {
           claw.setPower(0);
         }
-        // telemetry.addData("arm1", arm1.getPower());
-        // telemetry.addData("m1", m1.getCurrentPower());
-        // telemetry.addData("m2", m2.getCurrentPower());
-        // telemetry.addData("m3", m3.getCurrentPower());
-        // telemetry.addData("m4", m4.getCurrentPower());
-        // telemetry.addData("ArmPot", armpot.getVoltage());
-        // telemetry.addData("ClawPot", clawpot.getVoltage());
         
         telemetry.addData("boost", SpeedupVal);
         telemetry.update();
       }
-      // Put run blocks here.
     }
-
-    androidTextToSpeech.close();
   }
 }
