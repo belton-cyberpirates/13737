8b32049c871e9f6484aaf04a8824563137eba4bd
qwertyuiopwm <wyatt.mcintosh@students.bsd124.org>
2023-11-20 12:57:18 -0600

    Section code to support comment anchors extension on VSC

--- mecanumDriveFieldCentric.java
+++ mecanumDriveFieldCentric.java
@@ -11,39 +11,62 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @TeleOp(name = "MecanumDriveFieldCentric")
 public class MecanumDriveFieldCentric extends LinearOpMode {
-	// Initialize base motor variables
-	private DcMotorEx MBackLeft;
-	private DcMotorEx MBackRight;
-	private DcMotorEx MFrontLeft;
-	private DcMotorEx MFrontRight;
+	//SECTION - Constants
+		//SECTION - Drive Contants
+			final int BASE_SPEED = 1500;
+			final double MAX_BOOST = 0.6; // boost maxes out at an additional 60% of the base speed
+		//!SECTION - End drive constants
 
-	// Initialize arm motor variables
-	private DcMotorEx MShoulderLeft;
-	private DcMotorEx MShoulderRight;
-	private DcMotorEx MElbowLeft;
-	private DcMotorEx MElbowRight;
+		//SECTION - Arm Constants
+			final int SHOULDER_MIN_POS = 140;
+			final int ELBOW_MIN_POS = 5;
+			final double SHOULDER_SPEED = 0.5;
+			final double ELBOW_SPEED = 0.5;
+			final double STRAFE_MULT = 1.41;
+		//!SECTION - End arm constats
+	//!SECTION - End constands
+	
+	//SECTION - Variable init
+		//SECTION - Drive Motors
+			private DcMotorEx MBackLeft;
+			private DcMotorEx MBackRight;
+			private DcMotorEx MFrontLeft;
+			private DcMotorEx MFrontRight;
+		//!SECTION - End Drive motors
 
-	// Initialize IMU variable
-	private IMU imu;
+		//SECTION - Arm Motors
+			private DcMotorEx MShoulderLeft;
+			private DcMotorEx MShoulderRight;
+			private DcMotorEx MElbowLeft;
+			private DcMotorEx MElbowRight;
+		//!SECTION - End arm motors
+
+		//NOTE - IMU
+		private IMU imu;
+	//!SECTION - End variable initialzation
 
 	@Override
 	public void runOpMode() throws InterruptedException {
-		// Attach drive motors
-		MBackLeft = hardwareMap.get(DcMotorEx.class, "m1");
-		MFrontLeft = hardwareMap.get(DcMotorEx.class, "m2");
-		MFrontRight = hardwareMap.get(DcMotorEx.class, "m3");
-		MBackRight = hardwareMap.get(DcMotorEx.class, "m4");
+		//SECTION - Define Variables
+			//SECTION - Drive motors
+				MBackLeft = hardwareMap.get(DcMotorEx.class, "m1");
+				MFrontLeft = hardwareMap.get(DcMotorEx.class, "m2");
+				MFrontRight = hardwareMap.get(DcMotorEx.class, "m3");
+				MBackRight = hardwareMap.get(DcMotorEx.class, "m4");
+			//!SECTION - End drive motors
 
-		// Attach arm motors
-		MShoulderLeft = hardwareMap.get(DcMotorEx.class, "left_shoulder");
-		MShoulderRight = hardwareMap.get(DcMotorEx.class, "right_shoulder");
-		MElbowLeft = hardwareMap.get(DcMotorEx.class, "left_elbow");
-		MElbowRight = hardwareMap.get(DcMotorEx.class, "right_elbow");
+			//SECTION - Arm Motors
+				MShoulderLeft = hardwareMap.get(DcMotorEx.class, "left_shoulder");
+				MShoulderRight = hardwareMap.get(DcMotorEx.class, "right_shoulder");
+				MElbowLeft = hardwareMap.get(DcMotorEx.class, "left_elbow");
+				MElbowRight = hardwareMap.get(DcMotorEx.class, "right_elbow");
+			//!SECTION - End arm motors
 
-		// Attach IMU
-		imu = hardwareMap.get(IMU.class, "imu");
+			//NOTE - IMU
+			imu = hardwareMap.get(IMU.class, "imu");
+		//!SECTION - End variable definitions
 
-		// Set the zero power behaviour, or what the motor should when at zero power
+		//NOTE - Set the zero power behaviour
 		MBackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 		MBackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 		MFrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -63,23 +86,28 @@ public class MecanumDriveFieldCentric extends LinearOpMode {
 		// Wait for the start button to be pressed
 		waitForStart();
 
-		// Reset robot heading on startup (not initialisation)
-		// MAKE SURE ROBOT IS FACING FORWARD BEFORE HITTING START!
+		
+		//NOTE - Reset robot heading on startup (not initialization)
+		//NOTE - MAKE SURE ROBOT IS FACING FORWARD BEFORE HITTING START!
 		imu.resetYaw();
 
 		while (opModeIsActive()) {
-			// Reset Yaw on start button press so that a restart is not needed if Yaw should be reset again.
+			//NOTE - Reset Yaw on start button press so that a restart is not needed if Yaw should be reset again.
 			if (gamepad1.start) {
 				imu.resetYaw();
 			}
 			
-			double leftStickXGP1 = gamepad1.left_stick_x;
-			double leftStickYGP1 = gamepad1.left_stick_y;
-			double rightStickXGP1 = gamepad1.right_stick_x;
-			double rightStickYGP1 = gamepad1.right_stick_y;
-
-			double leftStickYGP2 = gamepad2.left_stick_y;
-			double rightStickYGP2 = gamepad2.right_stick_y;
+			//SECTION GP1
+				double leftStickXGP1 = gamepad1.left_stick_x;
+				double leftStickYGP1 = gamepad1.left_stick_y;
+				double rightStickXGP1 = gamepad1.right_stick_x;
+				double rightStickYGP1 = gamepad1.right_stick_y;
+			//!SECTION End GP1
+			
+			//SECTION GP2
+				double leftStickYGP2 = gamepad2.left_stick_y;
+				double rightStickYGP2 = gamepad2.right_stick_y;
+			//!SECTION End GP2
 
 			double maxSpeed = calcMaxSpeed(gamepad1.right_trigger - gamepad1.left_trigger, BASE_SPEED, MAX_BOOST);
 
@@ -94,37 +122,42 @@ public class MecanumDriveFieldCentric extends LinearOpMode {
 				   Math.cos(botHeading) -
 				   leftStickYGP1 *
 				   Math.sin(botHeading);
-
 			double rotatedY =
 				   leftStickXGP1 *
 				   Math.sin(botHeading) +
 				   leftStickYGP1 *
 				   Math.cos(botHeading);
-
 			rotatedX *= STRAFE_MULT; // strafing is slower than rolling, bump speed
 
 			// Set the power of the wheels based off the new joystick coordinates
 			// y+x+stick <- [-1,1]
+			// Define caluclated stick values so we don't repeat the same equations
+			double YPX = rotatedY + rotatedX - rightStickXGP1;
+			double YMX = rotatedY - rotatedX - rightStickXGP1;
+
 			MBackLeft.setVelocity(
-				(rotatedY + rotatedX - rightStickXGP1) * maxSpeed
+				YPX * maxSpeed
 			);
 			MFrontLeft.setVelocity(
-				(rotatedY - rotatedX - rightStickXGP1) * maxSpeed
+				YMX * maxSpeed
 			);
 			MBackRight.setVelocity(
-				(-rotatedY + rotatedX - rightStickXGP1) * maxSpeed
+				-YPX * maxSpeed
 			);
 			MFrontRight.setVelocity(
-				(-rotatedY - rotatedX - rightStickXGP1) * maxSpeed
+				-YMX * maxSpeed
 			);
+			//!SECTION - End Base
 
-			// Set the power of the arm motors
+			//SECTION - Arms
+			//NOTE - Set the power of the arm motors
 			MShoulderLeft.setPower(-leftStickYGP2 * SHOULDER_SPEED);
 			MShoulderRight.setPower(leftStickYGP2 * SHOULDER_SPEED);
 			MElbowLeft.setPower(rightStickYGP2 * ELBOW_SPEED);
 			MElbowRight.setPower(-rightStickYGP2 * ELBOW_SPEED);
+			//!SECTION - End Arms
 
-			// Display velocity
+			//SECTION - Telemetry
 			telemetry.addData("Speed Mod:", maxSpeed);
 			telemetry.addData(
 				"Left Shoulder:",
@@ -139,6 +172,7 @@ public class MecanumDriveFieldCentric extends LinearOpMode {
 			telemetry.addData("Heading (radians):", botHeading);
 
 			telemetry.update();
+			//!SECTION - End telemetry
 		}
 	}
 
