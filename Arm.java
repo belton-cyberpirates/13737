3637279b96b088f1574907b8e3e78ca3ba24d6d8
Oshawhat <85427862+Oshawhaat@users.noreply.github.com>
2023-12-09 18:12:21 -0600

    redo arm class to work for new arm

--- Arm.java
+++ Arm.java
@@ -14,66 +14,62 @@ public class Arm {
   private DcMotorEx rightShoulder;
   private DcMotorEx leftElbow;
   private DcMotorEx rightElbow;
+  private DcMotorEx[] motors;
 
   public Arm(DcMotorEx leftShoulder, DcMotorEx rightShoulder, DcMotorEx leftElbow, DcMotorEx rightElbow) {
     this.leftShoulder = leftShoulder;
     this.rightShoulder = rightShoulder;
     this.leftElbow = leftElbow;
     this.rightElbow = rightElbow;
+
+    // reverse left motors
+    this.leftShoulder.setDirection(DcMotor.Direction.REVERSE);
+    this.leftElbow.setDirection(DcMotor.Direction.REVERSE);
+
+    // create list of motors to make code cleaner
+    this.motors = new DcMotorEx[]{this.leftShoulder, this.rightShoulder, this.leftElbow, this.rightElbow};
   }
   
-  public void DropArm(){
-    this.arm1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-    this.arm2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-    this.arm1.setPower(-0.95);
-    this.arm2.setPower(0.95);
+  public void DropArm() {
+    this.motors.forEach( (motor) -> { motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); } )
+    this.motors.forEach( (motor) -> { motor.setPower(-.5); } )
   }
 
   
   public void Initialize() {
-    this.arm1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-    this.arm2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-    this.arm1.setTargetPosition(0);
-    this.arm2.setTargetPosition(0);
-    this.arm1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-    this.arm2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    this.motors.forEach( (motor) -> { motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); } )
+    this.motors.forEach( (motor) -> { motor.setTargetPosition(0); } )
+    this.motors.forEach( (motor) -> { motor.setMode(DcMotor.RunMode.RUN_TO_POSITION); } )
   }
   
   private void setVelocity(int velocity) {
-    this.arm1.setVelocity(velocity);
-    this.arm2.setVelocity(velocity);
+    this.motors.forEach( (motor) -> { motor.setVelocity(velocity) } )
   }
   
   public void Move(int position) {
     this.setVelocity(Config.ARM_VELOCITY);
-    
-    this.arm1.setTargetPosition(position);
-    this.arm2.setTargetPosition(-position);
+    this.motors.forEach( (motor) -> { motor.setTargetPosition(position); } )
   }
   
 
 public void Move(int position, boolean waitForDone) {
     this.Move(position);
     
-    if (waitForDone){
+    if (waitForDone)
       WaitForMotors();
-    }
   }
 
-  public void Move(int position, boolean waitForDone, int newVelocity) {
-    this.setVelocity(newVelocity);
+  public void Move(int position, boolean waitForDone, int tempVelocity) {
+    this.setVelocity(tempVelocity);
 
-    this.arm1.setTargetPosition(position);
-    this.arm2.setTargetPosition(-position);
+    this.motors.forEach( (motor) -> { motor.setTargetPosition(position); } )
     
-    if (waitForDone){
+    if (waitForDone)
       WaitForMotors();
-    }
-
   }
   
   private void WaitForMotors() {
-    while (this.arm1.isBusy() || this.arm2.isBusy()) {}
+    while (this.leftShoulder.isBusy() || this.rightShoulder.isBusy() || this.leftElbow.isBusy() || this.rightElbow.isBusy()) {}
     this.setVelocity(Config.ARM_VELOCITY);
   }
 }
