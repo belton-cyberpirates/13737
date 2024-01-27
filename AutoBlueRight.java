9e00087657ce5c1225da591c7031ab7f6d24c985
Oshawhat <85427862+Oshawhaat@users.noreply.github.com>
2024-01-27 03:16:35 -0600

    remove uneeded comments, update claw methods, add camera code, score purple pixel on spike mark

--- AutoBlueRight.java
+++ AutoBlueRight.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import org.firstinspires.ftc.vision.tfod.TfodProcessor;
+import org.firstinspires.ftc.vision.VisionPortal;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
@@ -33,14 +35,16 @@ import org.firstinspires.ftc.teamcode.DriveMotors;
 import org.firstinspires.ftc.teamcode.Arm;
 import org.firstinspires.ftc.teamcode.Direction;
 import org.firstinspires.ftc.teamcode.Config;
+import org.firstinspires.ftc.teamcode.TensorflowCamera;
+
 
 
 @Autonomous(name = "AutoBlueRight")
 public class AutoBlueRight extends LinearOpMode {
-  private OpenCvCamera camera;
-  //private AprilTagDetectionPipeline aprilTagDetectionPipeline;
+  private TensorflowCamera camera;
   private DriveMotors driveMotors;
   private Arm arm;
+
   private Servo wrist;
   private Servo clawLeft;
   private Servo clawRight;
@@ -58,86 +62,12 @@ public class AutoBlueRight extends LinearOpMode {
   }
 
 
-  /**
-   * Attempt to recognize the AprilTag and return which tag we see
-   */
- // private int RunAprilTagDetection() {
-	// for (int numFramesWithoutDetection = 0; numFramesWithoutDetection < Config.MAX_NUM_FRAMES_NO_DETECTION; numFramesWithoutDetection++) {
-	//   // Calling getDetectionsUpdate() will only return an object if there was a new frame
-	//   // processed since the last time we called it. Otherwise, it will return null. This
-	//   // enables us to only run logic when there has been a new frame, as opposed to the
-	//   // getLatestDetections() method which will always return an object.
-	//   ArrayList<AprilTagDetection> detections = aprilTagDetectionPipeline.getDetectionsUpdate();
-
-	//   // add camera stats to telemetry
-	//   telemetry.addData("FPS", camera.getFps());
-	//   telemetry.addData("Overhead ms", camera.getOverheadTimeMs());
-	//   telemetry.addData("Pipeline ms", camera.getPipelineTimeMs());
-	//   telemetry.update();
-
-	//   // If we haven't seen a tag for a few frames, lower the decimation
-	//   // so we can hopefully pick one up if we're e.g. far back
-	//   if(numFramesWithoutDetection >= Config.NUM_FRAMES_BEFORE_LOW_DECIMATION) {
-	// 	  aprilTagDetectionPipeline.setDecimation(Config.DECIMATION_LOW);
-	//   }
-
-	//   if (detections == null || detections.size() == 0) {
-	// 	// pass if there's no new frame or we didn't detect anything
-	// 	sleep(20); // give the camera a chance to process the frame
-	// 	continue;
-	//   }
-
-	//   // If we're here, we have a new frame and we detected at least one tag
-	//   // turn on high decimation to increase the frame rate
-	//   aprilTagDetectionPipeline.setDecimation(Config.DECIMATION_HIGH);
-
-
-	//   AprilTagDetection detection = detections.get(0);
-	//   telemetry.addLine(String.format("\n!!---[[ DETECTED ID=%d]]---!!", detection.id));
-	//   telemetry.addLine(String.format("Translation X: %.2f", detection.pose.x));
-	//   telemetry.addLine(String.format("Translation Y: %.2f", detection.pose.y));
-	//   telemetry.addLine(String.format("Translation Z: %.2f", detection.pose.z));
-	//   telemetry.addLine(String.format("Rotation Yaw: %.2f", detection.pose.yaw));
-	//   telemetry.addLine(String.format("Rotation Pitch: %.2f", detection.pose.pitch));
-	//   telemetry.addLine(String.format("Rotation Roll: %.2f", detection.pose.roll));
-	//   telemetry.addLine(String.format("Attempts: %d", numFramesWithoutDetection));
-	//   telemetry.update();
-
-	//   return detection.id;
-	// }
-
-	// telemetry.addLine(String.format("!!---[[ NO TAG DETECTED AFTER %d ATTEMPTS ]]---!!", Config.MAX_NUM_FRAMES_NO_DETECTION));
-	// telemetry.update();
-	// return -1; // no tag detected; godspeed
- // }
-
-
+  
   /**
    * This function is executed when this Op Mode is initialized from the Driver Station.
    */
   @Override
   public void runOpMode() {
-	//WebcamName camName = hardwareMap.get(WebcamName.class, "Webcam 1");
-	//camera = OpenCvCameraFactory.getInstance().createWebcam(camName);
-	//aprilTagDetectionPipeline = new AprilTagDetectionPipeline(Config.TAGSIZE, Config.FX, Config.FY, Config.CX, Config.CY);
-
-	//camera.setPipeline(aprilTagDetectionPipeline);
-	//camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
-	// {
-	// 	@Override
-	// 	public void onOpened()
-	// 	{
-	// 		camera.startStreaming(800,448, OpenCvCameraRotation.UPRIGHT);
-	// 	}
-
-	// 	@Override
-	// 	public void onError(int errorCode)
-	// 	{
-	// 	  telemetry.addData("Error", "Camera failed to open with error " + errorCode);
-	// 	  telemetry.update();
-	// 	}
-	// });
-
 	// argument order *must* be fr-fl-bl-br
 	driveMotors = new DriveMotors(
 	  hardwareMap.get(DcMotorEx.class, "m3"),
@@ -155,68 +85,90 @@ public class AutoBlueRight extends LinearOpMode {
 	clawRight = hardwareMap.get(Servo.class, "clawRight");
 	wrist = hardwareMap.get(Servo.class, "wrist");
 
+	CloseClaw();
+	wrist.setPosition(0);
+	
+	//initTfod();
+	int position = camera.GetPropPos();
+	  
+	telemetry.addData("position", position);
+	telemetry.update();
+	
 	waitForStart();
 
 	if (opModeIsActive()) { // <----------------------------------------------------------------
 	  MotorSetup();
-	  arm.MoveShoulder(-250);
-	  driveMotors.Move(Direction.FORWARD, (int)(Config.TILE_LENGTH * 2.2));
-	  driveMotors.Turn(-90);
-	  sleep(15000);
-	  driveMotors.Move(Direction.FORWARD, (int)(Config.TILE_LENGTH * 3.5));
-	  driveMotors.Turn(90);
-	  arm.MoveShoulder(0);
-	  sleep(1000);
-	  OpenClaw(clawLeft, clawRight);
+	  
+	  switch(position) {
+		case 0:
+			// Move to the left spike mark
+			driveMotors.Move(Direction.FORWARD, (int)(Config.TILE_LENGTH * 1));
+			driveMotors.Turn(-45);
+			driveMotors.Move(Direction.FORWARD, (int)(Config.TILE_LENGTH * 0.2));
+			// Drop purple pixel
+			OpenClaw(false, true);
+			sleep(350);
+			// Move to a starting point for scoring / parking auto
+			driveMotors.Move(Direction.BACKWARD, (int)(Config.TILE_LENGTH * 0.2));
+			driveMotors.Turn(45);
+			driveMotors.Move(Direction.FORWARD, (int)(Config.TILE_LENGTH * 1.2));
+			// Rotate towards board
+			driveMotors.Turn(-90);
+			break;
+		case 1:
+			// Move to the center spike mark
+			driveMotors.Move(Direction.LEFT, (int)(Config.TILE_LENGTH * .2));
+			driveMotors.Move(Direction.FORWARD, (int)(Config.TILE_LENGTH * 1.11));
+			// Drop pixel
+			OpenClaw(false, true);
+			sleep(350);
+			// Move to a starting point for scoring / parking auto
+			driveMotors.Move(Direction.LEFT, (int)(Config.TILE_LENGTH * .8));
+			driveMotors.Move(Direction.BACKWARD, (int)(Config.TILE_LENGTH * .11));
+			// Rotate towards board
+			driveMotors.Turn(-90);
+			break;
+		case 2:
+			// Move to the right spike mark
+			driveMotors.Move(Direction.FORWARD, (int)(Config.TILE_LENGTH * .7));
+			driveMotors.Move(Direction.RIGHT, (int)(Config.TILE_LENGTH * .6));
+			// Drop pixel
+			OpenClaw(false, true);
+			sleep(350);
+			// Move to a starting point for scoring / parking auto
+			driveMotors.Move(Direction.LEFT, (int)(Config.TILE_LENGTH * .6));
+			driveMotors.Move(Direction.FORWARD, (int)(Config.TILE_LENGTH * 1.2))
+			// Rotate towards board
+			driveMotors.Turn(-90);
+			break;
+	  }
 	}
   }
 
 
-  private void Park(int target) {
-	telemetry.addData("Parking", String.format("PARKING IN SPOT %d", target));
-
-	switch(target) {
-	case 1:
-	  driveMotors.Move(Direction.BACKWARD, (int)(Config.TILE_LENGTH * .5));
-	  break;
-	  
-	case 2:
-	  driveMotors.Move(Direction.FORWARD, (int)(Config.TILE_LENGTH * .5));
-	  break;
-	  
-	case 3:
-	  driveMotors.Move(Direction.FORWARD, (int)(Config.TILE_LENGTH * 1.5));
-	  break;
-
-	default:
-	  telemetry.addLine(String.format("ERROR: Target %d not in range 1-3", target));
-	  //telemetry.addLine(String.format("PARKING IN DEFAULT SPOT (%d)", Config.DEFAULT_PARKING_SPOT));
-	  //Park(Config.DEFAULT_PARKING_SPOT);
-	  break;
-	}
-	telemetry.update();
-  }
-
-
-  /**
-   * Open the given claw(s)
-   */
-  public void OpenClaw(Servo... claws) {
-	  clawLeft.setPosition(Config.CLAW_LEFT_OPEN);
-	  clawRight.setPosition(Config.CLAW_RIGHT_OPEN);
-  }
-  
-  
   public void MoveWrist(double position) {
 	  wrist.setPosition(position);
   }
+  
+  
+  public void OpenClaw() {
+  	OpenClaw(true, true);
+  }
+  
+
+  public void OpenClaw(boolean openLeft, boolean openRight) {
+	  if (openLeft) clawLeft.setPosition(Config.CLAW_LEFT_OPEN);
+	  if (openRight) clawRight.setPosition(Config.CLAW_RIGHT_OPEN);
+  }
+  
+  
+  public void CloseClaw() {
+	  CloseClaw(true, true);
+  }
 
 
-  /**
-   * Close the given claw(s)
-   */
-  public void CloseClaw(Servo... claws) {
-	  clawLeft.setPosition(Config.CLAW_LEFT_CLOSE);
-	  clawRight.setPosition(Config.CLAW_RIGHT_CLOSE);
+  public void CloseClaw(boolean closeLeft, boolean closeRight) {
+	  if (closeLeft) clawLeft.setPosition(Config.CLAW_LEFT_CLOSE);
+	  if (closeRight) clawRight.setPosition(Config.CLAW_RIGHT_CLOSE);
   }
 }
