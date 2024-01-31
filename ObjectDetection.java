6fc160a9df0eafe892b0fccc2eeaf07670f44217
Oshawhaat <85427862+Oshawhaat@users.noreply.github.com>
2024-01-30 18:29:59 -0600

    change camera class to ObjectDetection

--- ObjectDetection.java
+++ ObjectDetection.java
@@ -22,37 +22,27 @@ import java.util.List;
 
 
 
-public class Camera extends LinearOpMode {
+public class ObjectDetection extends LinearOpMode {
 	private TfodProcessor tfod;
 	private VisionPortal visionPortal;
 
-	private static final int CameraResoX = 640;
-
-	// TFOD_MODEL_FILE points to a model file stored onboard the Robot Controller's storage,
-	private static final String TFOD_MODEL_FILE = "/sdcard/FIRST/tflitemodels/teampiece.tflite";
-
-	// Define the labels recognized in the model for TFOD (must be in training order!)
-	private static final String[] LABELS = {
-		"BLUE",
-		"RED",
-	};
 	
-
 	public void runOpMode() {}
 
-	public void Init(WebcamName camera) {
+
+	public void Initialize(WebcamName camera) {
 
 		// Create the TensorFlow processor by using a builder.
 		tfod = new TfodProcessor.Builder()
-			.setModelFileName(TFOD_MODEL_FILE)
-			.setModelLabels(LABELS)
+			.setModelFileName(Config.TFOD_MODEL_FILE)
+			.setModelLabels(Config.LABELS)
 			.build();
 
 		// Create the vision portal by using a builder.
 		VisionPortal.Builder builder = new VisionPortal.Builder();
 
 		// Set the camera (webcam vs. built-in RC phone camera).
-		builder.setCamera(camera);
+		builder.setCamera(hardwareMap.get(Config.CAMERA_NAME));
 
 		// Set and enable the processor.
 		builder.addProcessor(tfod);
@@ -61,19 +51,21 @@ public class Camera extends LinearOpMode {
 		visionPortal = builder.build();
 	}
 
+
 	public int GetPropPos() { // 0 = left, 1 = center, 2 = right
 		Recognition recognition = highestConfidence();
 
 		if (recognition != null) {
 			double propX = (recognition.getLeft() + recognition.getRight()) / 2 ;
 
-			if ((CameraResoX / 2) > propX) {
+			if ((Config.CAMERA_RESO_X / 2) > propX) {
 				return 1;
 			}
 			return 2;
 		}
 		return 0;
 	}
+
 
 	private Recognition highestConfidence() {
 		Recognition recognition = null;
@@ -98,5 +90,4 @@ public class Camera extends LinearOpMode {
 		}
 		return recognition;
 	}
-
 }
