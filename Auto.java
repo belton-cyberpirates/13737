e5a19c45ece211529b96045aca113dca69480148
Oshawhat <85427862+Oshawhaat@users.noreply.github.com>
2024-02-20 09:24:50 -0600

    change everything to be protected, add IMU, get heading in saveHeading

--- Auto.java
+++ Auto.java
@@ -26,25 +26,29 @@ import org.firstinspires.ftc.teamcode.ObjectDetection;
 import java.util.List;
 
 public abstract class Auto extends LinearOpMode {
-	public DriveMotors driveMotors;
-	public Arm arm;
-	public Intake intake;
-	public ObjectDetection camera;
+	protected DriveMotors driveMotors;
+	protected Arm arm;
+	protected Intake intake;
+	protected ObjectDetection camera;
+	protected IMU imu;
 	
 	/**
 	 * Initialize classes used by autos
 	 */
-	public void Initialize() {
+	protected void Initialize() {
 		driveMotors = new DriveMotors(this);
 		arm = new Arm(this);
 		intake = new Intake(this);
 		camera = new ObjectDetection(this);
+
+		imu = hardwareMap.get(IMU.class, "imu");
+		imu.resetYaw();
 	}
 
 	/**
 	 * Set reliable initial configuration for robot motors
 	 */
-	public void MotorSetup() {
+	protected void MotorSetup() {
 		intake.CloseClaws(0);
 		intake.MoveWrist(0);
 		arm.DropArm();
@@ -52,7 +56,9 @@ public abstract class Auto extends LinearOpMode {
 		arm.Initialize();
 	}
 
-	protected void saveHeading(double heading) {
+	protected void saveHeading() {
+		double heading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS)
+
 		FileWriter writer = new FileWriter("heading.text", true);
 		String strHeading = Double.toString(heading);
 
